1bf9aaea35042625c8e7aae243583ca6dd33054b
Harsh Raj Srivastav <114308429+Harsh-Srivastav123@users.noreply.github.com>
2023-09-20 22:53:05 +0530

    Added solution - LeetHub

--- Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.java
+++ Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.java
@@ -31,58 +31,25 @@ class GfG
 {
     int maxLen(int arr[], int n)
     {
-        // // Your code here
-        // HashMap<Long,Integer> hashMap=new HashMap<>();
-        // long sum=0;
-        // for(int i:arr){
-        //     sum+=i;
-        // }
-        // if(sum==0)
-        // {
-        //     return arr.length;
-        // }
-        // else{
-        //     sum=0;
-        // }
-      
-        // int maxLength=0;
-        // for(int i=0;i<arr.length;i++){
-        //     sum+=arr[i];
-        //     if(hashMap.containsKey(sum)){
-        //         maxLength=Math.max(maxLength,i-hashMap.get(sum));
-        //     }
-        //     else {
-        //         hashMap.put(sum,i );
-        //     }
-        // }
-        // return maxLength;
-        
-          // Creates an empty hashMap hM
-        HashMap<Integer, Integer> hM
-            = new HashMap<Integer, Integer>();
- 
-        int sum = 0; // Initialize sum of elements
-        int max_len = 0; // Initialize result
- 
-        // Traverse through the given array
-        for (int i = 0; i < arr.length; i++) {
-            // Add current element to sum
-            sum += arr[i];
- 
-            if (sum == 0)
-                max_len = i + 1;
- 
-            // Look this sum in hash table
-            Integer prev_i = hM.get(sum);
- 
-            // If this sum is seen before, then update
-            // max_len if required
-            if (prev_i != null)
-                max_len = Math.max(max_len, i - prev_i);
-            else // Else put this sum in hash table
-                hM.put(sum, i);
+        // Your code here
+        HashMap<Long,Integer> hashMap=new HashMap<>();
+        long sum=0;
+        int maxLength=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            if(sum==0){
+                maxLength=i+1;
+            }
+
+            if(hashMap.containsKey(sum)){
+                maxLength=Math.max(maxLength,i-hashMap.get(sum));
+            }
+            else {
+                hashMap.put(sum,i );
+            }
         }
- 
-        return max_len;
+        return maxLength;
+        
+
     }
 }
